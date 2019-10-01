85991de96c6ccd4b9bf5f3df73beefd6184713d6
lePtrx <leptrx@gmail.com>
2019-10-01 18:01:03 +0800

    added card shuffling algorithm

--- script.js
+++ script.js
@@ -8,46 +8,103 @@ class AudioController {
     this.bgMusic.volume = 0.5;
     this.bgMusic.loop = true;
   }
+
+  // To start BGM when game starts
   startMusic() {
     this.bgMusic.play();
   }
+
+  // To stop music when it is required and start music from top
   stopMusic() {
     this.bgMusic.pause();
     this.bgMusic.currentTime = 0;
   }
+
+  // Sound for when each card is flipped
   flip() {
     this.flipSound.play();
   }
+
+  // Sound for when there's a match
   match() {
     this.matchSound();
   }
+
+  // Stop bgm and plays victory bgm when game is won
   victory() {
     this.stopMusic();
     this.victorySound.play();
   }
+
+  // Stop bgm and plays game over bgm when timer is up
   gameOver() {
     this.stopMusic();
     this.gameOverSound.play();
   }
 }
 
+class MixOrMatch {
+  constructor(totalTime, cards) {
+    this.cardsArray = cards;
+    this.totalTime = totalTime;
+    this.timeRemaining = totalTime;
+    this.timer = document.getElementById("time-remaining");
+    this.ticker = document.getElementById("flips");
+    this.audioController = new AudioController();
+  }
+  startGame() {
+    this.cardToCheck = null;
+    this.totalClicks = 0;
+    this.timeRemaining = this.totalTime;
+    this.matchedCards = [];
+    this.busy = true;
+
+    this.shuffleCards();
+  }
+  flipCard(card) {
+    if (this.canFlipCard(card)) {
+      this.audioController.flip();
+      this.totalClicks++;
+      this.ticker.innerText = this.totalClicks;
+      card.classList.add("visible");
+    }
+  }
+
+  // Card shuffling algorithm
+  shuffleCards() {
+    for (let i = this.cardsArray.length - 1; i > 0; i--) {
+      let randIndex = Math.floor(Math.random() * (i + 1));
+      this.cardsArray[randIndex].style.order = i;
+      this.cardsArray[i].style.order = randIndex;
+    }
+  }
+
+  canFlipCard(card) {
+    return true;
+    // return !this.busy && !this.matchedCards.includes(card) && !==this.cardToCheck;
+  }
+}
+
 function ready() {
   let overlays = Array.from(document.getElementsByClassName("overlay-text"));
   let cards = Array.from(document.getElementsByClassName("card"));
+  let game = new MixOrMatch(100, cards);
 
   overlays.forEach(overlay => {
     overlay.addEventListener("click", () => {
       overlay.classList.remove("visible");
-      //game.startGame();
+      game.startGame();
     });
   });
 
   cards.forEach(card => {
     card.addEventListener("click", () => {
-      //game.flipCard(card);
+      game.flipCard(card);
     });
   });
 }
+
+// To check if all elements in index.html has been loaded before JS functions are loaded
 
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", ready());
